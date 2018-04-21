bb69a0ecdfee2f23840098958c6e765d1b11c6c8
Majorro <andrabaratrum007@gmail.com>
2018-04-21 21:46:45 +0300

    Correct_Third

--- check_password/main.py
+++ check_password/main.py
@@ -1,12 +1,13 @@
 from sys import argv
 import argparse
 import time
+import re
 
 
 def argparse_():
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', default='common_passwords.txt')
-    parser.add_argument('--password')
+    parser.add_argument('--password', default='computer,science228')
     args = parser.parse_args(argv[1:])
     return vars(args)
 
@@ -14,18 +15,47 @@ def open_file_and_check(filepath, password):
     try:
         with open(filepath) as data:
             for common_password in data:
-                if password == common_password:
+                if password == common_password.split('\n')[0]:
                     print('YOUR PASSWORD IS SHIT')
                     return 1
-                else:
-                    return 0
+            return 0
     except FileNotFoundError:
         print('Bad filepath, you bastard')
 
 def scoring(password):
-    
+    pattern_up = r'[A-Z]'
+    pattern_low = r'[a-z]'
+    pattern_digit = r'\d'
+    pattern_character = r'\W'
+    score = 0
+    if re.search(pattern_up, password):
+        score += 1
+    if re.search(pattern_low, password):
+        score += 1
+    if re.search(pattern_digit, password):
+        score += 1
+    if re.search(pattern_character, password):
+        score += 1
+    if len(password) < 4:
+        pass
+    elif 4 <= len(password) <= 7:
+        score += 1
+    elif 8 <= len(password) <= 13:
+        score += 2
+    elif 14 <= len(password) <= 19:
+        score += 3
+    else:
+        score += 4
+    print('safety of your password:', str(score)+'*')
+
 
 
 if __name__ == '__main__':
-    args = argparse.values()
-    print(open_file_and_check())
+    start = time.time()
+    args = list(argparse_().values())
+    if open_file_and_check(args[0], args[1]) == 0:
+        scoring(args[1])
+    else:
+        exit(0)
+    end = time.time()
+    print('time =', end - start)
